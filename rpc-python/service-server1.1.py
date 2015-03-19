21e0081ebfad7759d5017ef6ea032c86e269648c
briliantnugraha <briliantnugraha@gmail.com>
2015-03-19 09:39:17 +0700

    Revert "salah kirim wkwk"
    This reverts commit fa0aee7fade6d936d551484fa0f2bb133cd97535.

--- rpc-python/service-server1.1.py
+++ rpc-python/service-server1.1.py
@@ -1,28 +1,11 @@
-import cv2
-import pickle
-import xmlrpclib
-import platform
+import rpyc
 
-def server_version():
-	return platform.system() + '\n'+ platform.release()+ '\n'+ platform.version()
 
-def img_conversion(fileobj):
-	print "data masuk ke fungsi"
-	temp = pickle.loads(fileobj)
-	gray_img = cv2.cvtColor(temp[0], cv2.COLOR_BGR2GRAY)
+class MyService(rpyc.Service):
+    def exposed_line_counter(self, fileobj):
+        linenum = len(fileobj.readlines())
+        return linenum
 
-	kirim = []
-	kirim.append(gray_img)
-	kirim = pickle.dumps(kirim)
-	print "data berhasil dikirim"
-	return kirim
-
-print "Masukkan port address server: ",
-port = raw_input()
-
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-serv = SimpleXMLRPCServer(("localhost",int(port) ) )
-
-serv.register_function(img_conversion,"img_conversion")
-serv.register_function(server_version,"server_version")
-serv.serve_forever()
+from rpyc.utils.server import ThreadedServer
+t = ThreadedServer(MyService, port=18861)
+t.start()
